d6f208352d71290ad90216243bc8b404e6e13462
michelledayana <102559251+michelledayana@users.noreply.github.com>
2025-05-14 21:46:19 -0500

    Update app.py
    corrección puertos parte 2

--- app.py
+++ app.py
@@ -40,5 +40,7 @@ def incrementar():
 
 if __name__ == "__main__":
     init_db()
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+app.run(host="0.0.0.0", port=port)
+
 
